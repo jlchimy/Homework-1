803581bdd0bc6e4374baa0fa818cfea891037867
Jacob Louis Chimerine <jacobchim@Jacobs-MacBook-Pro.local>
2019-05-27 17:55:56 +0100

    code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  public myDateOfBirth: String;
 
   constructor() {}
+
+  ionViewDidEnter() {
+    this.myDateOfBirth = "2000-03-31";
+  }
+
+  pressMe() {
+    alert("Welcome to my page!");
+  }
+
+  submitInfo() {
+    alert("Great to know!");
+  }
+
+}
+
+export class ToastExample {
+
+  constructor(public toastController: ToastController) {}
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Your settings have been saved.',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Toast header',
+      message: 'Click to Close',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
 
 }
